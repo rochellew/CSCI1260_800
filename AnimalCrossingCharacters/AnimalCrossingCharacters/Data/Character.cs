7252ac49b3431f553c8e138d9162b8b85536c983
Rook <32746298+rochellew@users.noreply.github.com>
2024-02-14 12:38:44 -0500

    Add files via upload

---
+++ AnimalCrossingCharacters/AnimalCrossingCharacters/Data/Character.cs
@@ -0,0 +1,16 @@
+﻿namespace AnimalCrossingCharacters.Data
+{
+    public class Character
+    {
+        public string Name { get; set; } = string.Empty;
+        public string ImageURL { get; set; } = string.Empty;
+        public string Quote { get; set; } = string.Empty;
+
+        public Character(string name, string imageURL, string quote)
+        { 
+            Name = name;
+            ImageURL = imageURL;
+            Quote = quote;
+        }
+    }
+}
